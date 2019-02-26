c52648d0abcf99bae427837cde1272550ce6102c
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2019-02-26 18:22:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,31 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
+
 
 class Scraper
 
+attr_accessor :students
+
   def self.scrape_index_page(index_url)
-    
-  end
+    doc = Nokogiri::HTML(open(index_url))
+     scraped_students = []
 
-  def self.scrape_profile_page(profile_url)
-    
+     doc.css(".student-card").each do |student|
+      scraped_students << {
+        name: student.css("h4.student-name").text,
+        location: student.css("p.student-location").text,
+        profile_url: "http://students.learn.co/" + student.css("a").attribute("href")
+      }
+    end
+    scraped_students
   end
-
+ 
+ def self.scrape_profile_page(profile_url)
+    
+ end  
+ 
 end
+
+
 

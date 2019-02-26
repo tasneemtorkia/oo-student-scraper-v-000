60e1fd1cd775f92f9d50800ec724ef95bb84e943
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2019-02-26 18:31:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,29 +22,7 @@ attr_accessor :students
   end
  
  def self.scrape_profile_page(profile_url)
-   doc = Nokogiri::HTML(open(profile_url))
-  end	    student = Hash.new
-
-
-     social_icons = doc.css("div.social-icon-container a").collect {|x| x.attribute("href").value}
-    social_icons.each do |social_icon|
-      if social_icon.include?("linkedin")
-        student[:linkedin] = social_icon
-      elsif social_icon.include?("github")
-        student[:github] = social_icon
-      elsif social_icon.include?("twitter")
-        student[:twitter] = social_icon
-      else
-        student[:blog] = social_icon
-      end
-    end
-    if doc.css("div.bio-content.content-holder div.description-holder p")
-      student[:bio] = doc.css("div.bio-content.content-holder div.description-holder p").text
-    end
-    if doc.css(".profile-quote")
-      student[:profile_quote] = doc.css(".profile-quote").text
-    end
-    student
+    
  end  
  
 end
